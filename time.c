c94268374888613673716ae69696acc8168fd506
sameer3841 <sameer.patelde@gmail.com>
2023-10-26 16:05:31 +0000

    fixed time.c

--- time.c
+++ time.c
@@ -2,7 +2,7 @@
 
 double elapsed_time(struct timeval* start_time, struct timeval* end_time){
     //TODO: return the difference between end_time and start_time.
-    double startsec = start_time->tv_sec, endsec = end_time->tv_sec,
-    startnsec = start_time->tv_usec, endnsec = end_time->tv_usec;
-    return (endsec - startsec) * 1000000 + (endnsec - startnsec);
+    double startsec = start_time->tv_sec + (start_time->tv_usec / 1000000), 
+    endsec = end_time->tv_sec + (end_time->tv_usec / 1000000);
+    return endsec - startsec;
 }
